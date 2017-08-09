129284c945b4e49bf68515f976861f16cf2b4671
RodrigoKM <rodrigokoythi@hotmail.com>
2017-08-09 18:52:01 -0300

    layout mobile fix

--- js/init.js
+++ js/init.js
@@ -7,9 +7,5 @@
     $('.button-collapse').sideNav({
       closeOnClick: true
     });
-
-    function showModalandUpScreen() {
-      $("html, body").animate({ scrollTop: 0 }, 600);
-    }
   }); // end of document ready
 })(jQuery); // end of jQuery name space
