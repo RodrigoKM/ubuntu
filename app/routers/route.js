94de379e715a7aafaba88e53529051de19e02adf
RodrigoKM <rodrigokoythi@hotmail.com>
2017-08-09 02:03:30 -0300

    fluxo das paginas

--- app/routers/route.js
+++ app/routers/route.js
@@ -13,6 +13,18 @@
                 templateUrl: 'app/home/home.html',
                 //controller: 'HomeController',
                 //controllerAs: 'vm'
+            }).state('sobre', {
+                url: '/sobre',
+                templateUrl: 'app/sobre/sobre.html',
+            }).state('contatos', {
+                url: '/contatos',
+                templateUrl: 'app/contatos/contatos.html',
+            }).state('projetos', {
+                url: '/projetos',
+                templateUrl: 'app/projetos/projeto.html',
+            }).state('queroajudar', {
+                url: '/quero-ajudar',
+                templateUrl: 'app/queroajudar/quero-ajudar.html',
             });
 
         $urlRouterProvider.otherwise('/');
--- js/init.js
+++ js/init.js
@@ -1,7 +1,11 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-
+(function ($) {
+  $(function () {
+    $('.dropdown-button').dropdown({
+      hover: true,
+      belowOrigin: true
+    });
+    $('.button-collapse').sideNav({
+      closeOnClick: true
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
