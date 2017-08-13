3b71fb073dd01a9d07148e6d9bd77e9c295de047
RodrigoKM <rodrigokoythi@hotmail.com>
2017-08-13 01:30:56 -0300

    layout pack

--- js/init.js
+++ js/init.js
@@ -1,8 +1,14 @@
 (function ($) {
   $(function () {
     $('.dropdown-button').dropdown({
-      hover: true,
-      belowOrigin: true
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: true, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
     });
     $('.button-collapse').sideNav({
       closeOnClick: true
