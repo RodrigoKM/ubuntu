24781bda169438eaf277fe8005a387208e4dd966
RodrigoKM <rodrigokoythi@hotmail.com>
2017-08-30 23:37:52 -0300

    imagens menores

--- app/services/galeria.service.js
+++ app/services/galeria.service.js
@@ -16,61 +16,61 @@
 
       function getFotos() {
          return [{
-               img: 'img/29-08/img1.jpeg',
+               img: 'img/29-08/img1.jpg',
             },
             {
-               img: 'img/29-08/img2.jpeg',
+               img: 'img/29-08/img2.jpg',
             },
             {
-               img: 'img/29-08/img18.jpeg',
+               img: 'img/29-08/img18.jpg',
             },
             {
-               img: 'img/29-08/img19.jpeg',
+               img: 'img/29-08/img19.jpg',
             },
             {
-               img: 'img/29-08/img5.jpeg',
+               img: 'img/29-08/img5.jpg',
             },
             {
-               img: 'img/29-08/img6.jpeg',
+               img: 'img/29-08/img6.jpg',
             },
             {
-               img: 'img/29-08/img7.jpeg',
+               img: 'img/29-08/img7.jpg',
             },
             {
-               img: 'img/29-08/img8.jpeg',
+               img: 'img/29-08/img8.jpg',
             },
             {
-               img: 'img/29-08/img9.jpeg',
+               img: 'img/29-08/img9.jpg',
             },
             {
-               img: 'img/29-08/img10.jpeg',
+               img: 'img/29-08/img10.jpg',
             },
             {
-               img: 'img/29-08/img11.jpeg',
+               img: 'img/29-08/img11.jpg',
             },
             {
-               img: 'img/29-08/img12.jpeg',
+               img: 'img/29-08/img12.jpg',
             },
             {
-               img: 'img/29-08/img13.jpeg',
+               img: 'img/29-08/img13.jpg',
             },
             {
-               img: 'img/29-08/img14.jpeg',
+               img: 'img/29-08/img14.jpg',
             },
             {
-               img: 'img/29-08/img15.jpeg',
+               img: 'img/29-08/img15.jpg',
             },
             {
-               img: 'img/29-08/img16.jpeg',
+               img: 'img/29-08/img16.jpg',
             },
             {
-               img: 'img/29-08/img17.jpeg',
+               img: 'img/29-08/img17.jpg',
             },
             {
-               img: 'img/29-08/img4.jpeg',
+               img: 'img/29-08/img4.jpg',
             },
             {
-               img: 'img/29-08/img3.jpeg',
+               img: 'img/29-08/img3.jpg',
             }
          ]
       }
