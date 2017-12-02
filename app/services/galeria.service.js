092ad1204afffd17f7716fc661ea445927c39d91
Rodrigo Mizokami <rodrigokoythi@hotmail.com>
2017-12-02 19:53:59 -0200

    several changes

--- app/services/galeria.service.js
+++ app/services/galeria.service.js
@@ -1,78 +1,101 @@
-(function () {
-   'use strict';
+(function() {
+    'use strict';
 
-   angular
-      .module('app')
-      .factory('galeriaService', galeriaService);
+    angular.module('app').factory('galeriaService', galeriaService);
 
-   galeriaService.$inject = [];
+    galeriaService.$inject = [];
 
-   function galeriaService() {
-      var service = {
-         getFotos: getFotos
-      }
+    function galeriaService() {
+        var service = {
+            getFotos: getFotos
+        };
 
-      return service;
+        return service;
 
-      function getFotos() {
-         return [{
-               img: 'img/29-08/img1.jpg',
-            },
-            {
-               img: 'img/29-08/img2.jpg',
-            },
-            {
-               img: 'img/29-08/img18.jpg',
-            },
-            {
-               img: 'img/29-08/img19.jpg',
-            },
-            {
-               img: 'img/29-08/img5.jpg',
-            },
-            {
-               img: 'img/29-08/img6.jpg',
-            },
-            {
-               img: 'img/29-08/img7.jpg',
-            },
-            {
-               img: 'img/29-08/img8.jpg',
-            },
-            {
-               img: 'img/29-08/img9.jpg',
-            },
-            {
-               img: 'img/29-08/img10.jpg',
-            },
-            {
-               img: 'img/29-08/img11.jpg',
-            },
-            {
-               img: 'img/29-08/img12.jpg',
-            },
-            {
-               img: 'img/29-08/img13.jpg',
-            },
-            {
-               img: 'img/29-08/img14.jpg',
-            },
-            {
-               img: 'img/29-08/img15.jpg',
-            },
-            {
-               img: 'img/29-08/img16.jpg',
-            },
-            {
-               img: 'img/29-08/img17.jpg',
-            },
-            {
-               img: 'img/29-08/img4.jpg',
-            },
-            {
-               img: 'img/29-08/img3.jpg',
-            }
-         ]
-      }
-   }
+        function getFotos() {
+            return [
+                {
+                    img: 'img/29-08/img1.png'
+                },
+                {
+                    img: 'img/29-08/img2.png'
+                },
+                {
+                    img: 'img/29-08/img18.png'
+                },
+                {
+                    img: 'img/29-08/img19.png'
+                },
+                {
+                    img: 'img/29-08/img5.png'
+                },
+                {
+                    img: 'img/29-08/img6.png'
+                },
+                {
+                    img: 'img/29-08/img7.png'
+                },
+                {
+                    img: 'img/29-08/img8.png'
+                },
+                {
+                    img: 'img/29-08/img9.png'
+                },
+                {
+                    img: 'img/29-08/img10.png'
+                },
+                {
+                    img: 'img/29-08/img11.png'
+                },
+                {
+                    img: 'img/29-08/img12.png'
+                },
+                {
+                    img: 'img/29-08/img13.png'
+                },
+                {
+                    img: 'img/29-08/img14.png'
+                },
+                {
+                    img: 'img/29-08/img15.png'
+                },
+                {
+                    img: 'img/29-08/img16.png'
+                },
+                {
+                    img: 'img/29-08/img17.png'
+                },
+                {
+                    img: 'img/29-08/img4.png'
+                },
+                {
+                    img: 'img/29-08/img3.png'
+                },
+                {
+                    img: 'img/01-12/img7.jpeg'
+                },
+                {
+                    img: 'img/01-12/img2.jpeg'
+                },
+                {
+                    img: 'img/01-12/img3.jpeg'
+                },
+                {
+                    img: 'img/01-12/img4.jpeg'
+                },
+                {
+                    img: 'img/01-12/img5.jpeg'
+                },
+                {
+                    img: 'img/01-12/img6.jpeg'
+                },
+                {
+                    img: 'img/01-12/img1.jpeg'
+                },
+                {
+                    img: 'img/01-12/img8.jpeg'
+                }
+            ];
+        }
+    }
 })();
